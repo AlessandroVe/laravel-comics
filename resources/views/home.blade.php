6b36e29b5a18752d27052ec69ee1b4b7c8da1f77
AlessandroVe <aveneziano536@gmail.com>
2021-09-02 15:02:50 +0200

    .

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.base')
+
+@section('title', 'HomePage')
+
+@section('content')
+    <ul>
+        <li>prodotto1</li>
+        <li>prodotto2</li>
+        <li>prodotto3</li>
+        <li>prodotto4</li>
+    </ul>
+@endsection
