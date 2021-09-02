3cf9c1f7f7cc5065854342453262d78e1242865a
AlessandroVe <aveneziano536@gmail.com>
2021-09-02 17:31:28 +0200

    .

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -5,3 +5,26 @@
     <span>ADDITIONAL DC SITES</span>
 
 </div>
+<div class="container nav-top">
+    <div class="row">
+        <div class="col logo-nav">
+            <img src="" alt="">
+        </div>
+        <ul class="col d-flex">
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+            <li>s</li>
+        </ul>
+        <div class="col">
+            <input type="seach">
+        </div>
+    </div>
+
+</div>
