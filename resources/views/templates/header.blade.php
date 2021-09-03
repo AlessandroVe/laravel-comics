33d009f2bf2fee6731fe6963b4e35f2f8c054ff1
AlessandroVe <aveneziano536@gmail.com>
2021-09-03 10:33:21 +0200

    .

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -6,25 +6,26 @@
 
 </div>
 <div class="container nav-top">
-    <div class="d-flex align-items-center">
-        <div class=" logo-nav">
+    <div class="row align-items-center">
+        <div class="logo-nav">
             <img src="images/dc-logo.png" alt="">
         </div>
-        <ul class=" d-flex">
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
-            <li>s</li>
+        <ul class=" col d-flex nav-items">
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
         </ul>
-        <div class="">
-            <input type="seach">
+        <div class="search">
+            <input type="search">
         </div>
     </div>
 
 </div>
+<div class="big-bannner"></div>
