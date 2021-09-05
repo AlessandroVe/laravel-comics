ef0e0253f6ec16d98cd138069d9f52e3644b1cfa
AlessandroVe <aveneziano536@gmail.com>
2021-09-06 01:28:24 +0200

    .

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,1 +1,70 @@
-IO SONO FOOTER
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class=" row col-6">
+              <div class="col-4">
+                    <ul>
+                        <li>DC COMICS</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li>SHOP</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+              </div>
+              <div class="col-4">
+                <ul>
+                    <li>DC</li>
+                    <li>Terms of use</li>
+                    <li>Privacy policy (new)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact us</li>
+                </ul>
+              </div>
+              <div class="col-4">
+                <ul>
+                    <li>SITES</li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids
+                    <li>DC Universe</li>
+                    <li>DC Power Vise</li>
+                </ul>
+              </div>
+              <div class=" col-12">
+                <p>All Site Content TM and © 2020 DC Enterteinment,unless otherwise <a href="">notes here</a>. All rights reserved <a href="">Cookies Settings</a></p>
+              </div>
+            </div>
+            <div class="col-6 logo-footer">sss</div>
+        </div>
+    </div>
+    <div class="row  last-banner  ">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="sign-up">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="our-social-networks">
+                <span>FOLLOW US</span>
+                <img src="/images/footer-facebook.png" alt="">
+                <img src="/images/footer-periscope.png" alt="">
+                <img src="/images/footer-pinterest.png" alt="">
+                <img src="/images/footer-twitter.png" alt="">
+                <img src="/images/footer-youtube.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
