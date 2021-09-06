d52b8bd485c3d6e737ee3ac485257d62a3c1ae12
AlessandroVe <aveneziano536@gmail.com>
2021-09-06 03:00:25 +0200

    .

--- resources/views/templates/shop-bar.blade.php
+++ resources/views/templates/shop-bar.blade.php
@@ -1,7 +1,28 @@
-<div class="row shop-bar">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+
+<div class="row text-white shop-bar">
+    <div class="container">
+        <div class="items-bar d-flex align-items-center">
+            <div class="item-bar">
+                <img src="/images/buy-comics-digital-comics.png" alt="">
+                <span> DIGITAL COMICS</span>
+        </div>
+            <div class="item-bar">
+                <img src="/images/buy-comics-merchandise.png" alt="">
+                <span> DC MERCHANDISE</span>
+        </div>
+            <div class="item-bar">
+                <img src="/images/buy-comics-subscriptions.png" alt="">
+                <span> SUBSCRIPTION</span>
+        </div>
+            <div class="item-bar">
+                <img src="/images/buy-comics-shop-locator.png" alt="">
+                <span> COMIC SHOP LOCATOR</span>
+
+        </div>
+            <div class="item-bar">
+                <img src="/images/buy-dc-power-visa.svg" alt="">
+                <span> DC POWER VISA</span>
+        </div>
+        </div>
     </div>
+</div>
